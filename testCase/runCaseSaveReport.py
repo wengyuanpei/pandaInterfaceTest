27cdd790ef9348a35d18d676ec9d5ef1efd96677
wengyuanpei <wengyuanpei@163.com>
2022-08-30 14:24:15 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ testCase/runCaseSaveReport.py
@@ -0,0 +1,18 @@
+import unittest
+from pandaInterfaceTest.testCase.interfacecase_for_web import *
+from pandaInterfaceTest.common.resultPath import *
+from BeautifulReport import BeautifulReport as bf
+
+
+
+suite=unittest.TestSuite()
+suite.addTest(unittest.makeSuite(TestPandaInterface))
+
+report_path1 = saveReportPath()
+report_path = report_path1 + '.html'
+run = bf(suite) #实例化BeautifulReport模块
+repost_path=saveReportPath()
+run.report(filename="test",description='这个描述参数是必填的',report_dir=repost_path)
+
+
+
