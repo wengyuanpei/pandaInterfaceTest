f8ed893c0f666a7dacbc22a4356c2bc2929646b5
wengyuanpei <wengyuanpei@163.com>
2022-08-30 15:48:08 +0800

    报告描述更新

--- testCase/runCaseSaveReport.py
+++ testCase/runCaseSaveReport.py
@@ -12,7 +12,7 @@ report_path1 = saveReportPath()
 report_path = report_path1 + '.html'
 run = bf(suite) #实例化BeautifulReport模块
 repost_path=saveReportPath()
-run.report(filename="test",description='这个描述参数是必填的',report_dir=repost_path)
+run.report(filename="这是熊猫平板的接口测试报告",description='该报告汇总所有的接口测试',report_dir=repost_path)
 
 
 
