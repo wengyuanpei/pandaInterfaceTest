bda7a8b143c73e4bead22bc5ed81ddd399b585bf
wengyuanpei <wengyuanpei@163.com>
2022-08-31 18:32:07 +0800

    mqtt

--- pressurePandaServer/mqtt-connet.py
+++ pressurePandaServer/mqtt-connet.py
@@ -8,7 +8,7 @@ HOST = broker_url
 PORT =port
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code :0 表示连接成功  其它表示失败" + str(rc))
+    print("Connected with result code :0 表示连接成功  其它表示失败，rc：" + str(rc))
     client.subscribe("chanel_01")
 
 
@@ -17,13 +17,14 @@ def main():
     client.connect(HOST, PORT, 60)
     client.on_connect = on_connect
     client.username_pw_set(client_id,password='token')
-    client.loop_forever()
+    client.loop_forever()  #长连接
 
 
 if __name__ == '__main__':
     main()
 
 
+# 代码结构 1、定义数据库插入方法关联cilentId和用户  2、定义一个循环持续连接并保持连接
 
 
 
