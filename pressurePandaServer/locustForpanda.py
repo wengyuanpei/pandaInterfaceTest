9b4d93ae954926efc55994c6e93082b5bb703020
wengyuanpei <wengyuanpei@163.com>
2022-09-13 22:52:25 +0800

    mqtt  token

--- pressurePandaServer/locustForpanda.py
+++ pressurePandaServer/locustForpanda.py
@@ -3,7 +3,7 @@ from locust import HttpUser, task, between, events
 from locust.contrib.fasthttp import FastHttpUser
 import random
 import paho.mqtt.client as mqtt
-
+from pandaInterfaceTest.parameter.getMqttToken import getmqtttoeken
 
 
 @events.test_start.add_listener
@@ -18,22 +18,27 @@ def on_test_stop(**kwargs):
 
 class TestTask(HttpUser):
     wait_time = between(1, 5)
-    # broker_url = 'emqx-dev.xiongmaoboshi.com'
+    broker_url = 'emqx-dev.xiongmaoboshi.com'
 
     def on_start(self):
+        broker_url = 'emqx-dev.xiongmaoboshi.com'
         print('这是SETUP，每次实例化User前都会执行！')
-        #随机生成一个cltid插入数据到数据库
+        mqttpara = getmqtttoeken()
+        self.mqtttoeken = mqttpara[0]
+        self.mqttsn = mqttpara[1]
+        self.broker_url = 'emqx-dev.xiongmaoboshi.com'
+        self.port = 1883
 
 
-
-
-
+        def on_connect(client, userdata, flags, rc):
+            print("Connected with result code :0 表示连接成功  其它表示失败，rc：" + str(rc))
+            client.subscribe("chanel_01")
 
     @task(1)
     def getBaidu(self):
         broker_url = 'emqx-dev.xiongmaoboshi.com'
         port = 1883
-        client_id = str(random.randint(0, 1000)) + 'test'
+        self = str(random.randint(0, 1000)) + 'test'
         HOST = broker_url
         PORT = port
         def on_connect(client, userdata, flags, rc):
@@ -44,9 +49,8 @@ class TestTask(HttpUser):
             client = mqtt.Client(client_id)
             client.connect(HOST, PORT, 60)
             client.on_connect = on_connect
-            client.username_pw_set(client_id, password='token')
-            client.loop_forever()
-
+            client.username_pw_set(client_id, password=self.mqtttoeken)
+            client.loop_forever()  # 长连接
 
     def on_stop(self):
         print('这是TEARDOWN，每次销毁User实例时都会执行！')
