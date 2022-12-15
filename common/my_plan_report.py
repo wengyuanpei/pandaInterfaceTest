ecc27989fda3e52caee1ad99fb56bf2f3cabac0a
wengyuanpei <wengyuanpei@163.com>
2022-12-15 18:45:24 +0800

    完成计划上报方法修改

--- common/my_plan_report.py
+++ common/my_plan_report.py
@@ -1,33 +1,33 @@
 from time import sleep
 import requests
 
-def myplanreport(S1_words,S1_lesson,S1_video,url,header,uid):
+def myplanreport(S1_words,S1_lesson,S1_video,url,header,uid,event_id,s_time):
     for i in range(3):
         print('这是S1学习')
         if i == 0:
             S1_words = S1_words
             for i_S1_w in S1_words:
-                for i_S1_w_event_id in (1, 2, 3):
+                for i_S1_w_event_id in event_id:
                     data = {"event_id": i_S1_w_event_id, "user_plan_id": i_S1_w, "uid": uid}
                     sleep(2)
                     print('请求参数：', data)
                     reqsts = requests.post(url=url, headers=header, json=data)
-                    print('S1字卡学习完成上报：', reqsts.text)
+                    print(str(s_time)+'字卡学习完成上报：', reqsts.text)
         if i == 1:
             S1_lesson = S1_lesson
             for i_S1_w in S1_words:
-                for i_S1_w_event_id in (1, 2, 3):
+                for i_S1_w_event_id in event_id:
                     data = {"event_id": i_S1_w_event_id, "user_plan_id": i_S1_w, "uid": uid}
                     sleep(2)
                     print('请求参数：', data)
                     reqsts = requests.post(url=url, headers=header, json=data)
-                    print('S1古诗学习完成上报：', reqsts.text)
+                    print(str(s_time)+'古诗学习完成上报：', reqsts.text)
         if i == 2:
             S1_video = S1_video
             for i_S1_w in S1_words:
-                for i_S1_w_event_id in (1, 2, 3):
+                for i_S1_w_event_id in event_id:
                     data = {"event_id": i_S1_w_event_id, "user_plan_id": i_S1_w, "uid": uid}
                     sleep(2)
                     print('请求参数：', data)
                     reqsts = requests.post(url=url, headers=header, json=data)
-                    print('S1视频学习完成上报：', reqsts.text)
+                    print(str(s_time)+'视频学习完成上报：', reqsts.text)
