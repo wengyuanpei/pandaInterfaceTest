e75ac4bf49aa0028ec1b179fd5c77df98f1a2f63
wengyuanpei <wengyuanpei@163.com>
2023-01-13 18:50:15 +0800

    [faker]随机数据生成

---
+++ common/userAgent.py
@@ -0,0 +1,31 @@
+from faker import *
+
+#随机生成请求头
+def user_agent():
+    fc = Factory.create()
+    user_agt=fc.user_agent()
+    # print(user_agt)
+    return user_agt
+
+def phone_num():
+    fake=Faker(locale='zh_CN') # 初始化，指定生成中文格式数据
+    phone = fake.phone_number()
+    return  phone
+    # print(phone)
+
+def manname_num():
+    fake = Faker(locale='zh_CN')  # 初始化，指定生成中文格式数据
+    name=fake.name()
+    return name
+
+def cart_num():
+    fake = Faker(locale='zh_CN')
+    cart_num=fake.ssn()
+    print(cart_num)
+
+
+
+
+if __name__ == '__main__':
+    manname_num()
+    cart_num()
