7165d92b4167b784cea1ddffd6fd7b777c213dfb
wengyuanpei <wengyuanpei@163.com>
2023-02-25 16:29:43 +0800

    [code]修改听立机定级错误

--- testCase/tlj_dj_test.py
+++ testCase/tlj_dj_test.py
@@ -15,19 +15,19 @@ a=1
 while True:
     auth_end = getauth()[0]   #令牌
     phone_end = getauth()[1]  #手机号
-    uid_end=getauth()[2]      #uid
+    uid_end=int(getauth()[2])     #uid
     print("uid:",uid_end)
     print("auth_end:",auth_end)
     # print("uid:",uid_end)
 
     header = {"Authorization":auth_end}
-    data_set_ch={"level":"S4","type":2,"week_plan":"1,2,3,4,5,6,7","uid":uid_end}
+    data_set_ch={"level":"S4","type":int(2),"week_plan":"1,2,3,4,5,6,7","uid":uid_end}
 
     #首次定级
     req_set=requests.post(url=url_firstset_interface,json=data_set_ch,headers=header)
     print("req_set:",req_set.text)
 
-    if req_set.json()["code"]==0:
+    if req_set.json()["code"]==str(200):
         print("英语定级成功")
     else:
         print("英语定级失败,req_set.status_code:",req_set.text)
@@ -38,11 +38,11 @@ while True:
 
     level_list = ["aa", "A", "B", "C", "D", "E", "F", "G"]
     le = random.choice(level_list)
-    data_set_en={"level": le, "type": 1, "week_plan": "1,2,3,4,5,6,7", "uid": uid_end}
+    data_set_en={"level": le, "type": int(1), "week_plan": "1,2,3,4,5,6,7", "uid": uid_end}
     req_set_en=requests.post(url=url_firstset_interface,json=data_set_en,headers=header)
     print("req_set_en.text:",req_set_en.text)
 
-    if req_set_en.json()["code"]==0:
+    if req_set_en.json()["code"]==str(200):
         print("语文定级成功")
     else:
         print("语文定级失败,req_set.status_code:",req_set_en.text)
@@ -59,7 +59,7 @@ while True:
         "uid": uid_end,
         "week_plan": "1,2,3,4,5",
         "study_time": "09:00",
-        "type": 2,
+        "type": int(2),
         "level": "S1"
     }
 
@@ -72,7 +72,7 @@ while True:
     req_e=requests.post(url=url_reset_interface,json=data_e,headers=header)
     print("req_e.text:",req_e.text)
     time.sleep(1)
-    if req_e.json()["code"]==0:
+    if req_e.json()["code"]==str(200):
         print("英语重定级成功")
     else:
         print("英语定级失败,req_set.status_code:",req_e.text)
@@ -83,7 +83,7 @@ while True:
 
     req_c=requests.post(url=url_reset_interface,json=data_c,headers=header)
     print("req_c.text:", req_c.text)
-    if req_c.json()["code"]==0:
+    if req_c.json()["code"]==str(200):
         print("语文重定级成功")
     else:
         print("语文定级失败,req_set.status_code:",req_c.text)
@@ -92,5 +92,5 @@ while True:
     a+=1
 
     print(a)
-    if a==2:
+    if a==5000:
         break
