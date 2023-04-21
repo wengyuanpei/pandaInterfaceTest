006b9e30379deeeacfa39a08610fdf105ac7fbb9
wengyuanpei <wengyuanpei@163.com>
2023-04-21 16:57:20 +0800

    [interface]excel 计划检查

---
+++ testCase/excel_del_open.py
@@ -0,0 +1,35 @@
+# import openpyxl
+#
+# excel_dir='C:\Users\zhang\Desktop\无标题.xls'
+import json
+import os
+import openpyxl
+
+path = r"C:\Users\zhang\Desktop"
+os.chdir(path)  # 修改工作路径
+
+workbook = openpyxl.load_workbook('计划报表.xlsx')  # 返回一个workbook数据类型的值
+sheet = workbook.active  # 获取活动表
+# print('当前活动表是：')
+# print(sheet)
+
+cell = sheet['D2:D1056']  # 获取A1到A5的数据
+
+# print(cell)
+
+list_data=[]
+day=0
+# 打印A1到A5的数据
+for i in cell:
+    day+=1
+    for j in i:
+        json_data=json.loads(j.value)
+        for ii in json_data["audio_book_ids"]:
+            if ii>2950:
+                print("第",day,"天",ii)
+
+
+
+
+
+
