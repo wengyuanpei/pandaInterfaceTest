151e7d068c9e4e22f637345adabc95a9ec6c6673
wengyuanpei <wengyuanpei@163.com>
2023-04-25 15:47:29 +0800

    [interface] 英语dev计划脚本增加

--- testCase/excel_del_open.py
+++ testCase/excel_del_open.py
@@ -1,9 +1,9 @@
-# import openpyxl
-#
-# excel_dir='C:\Users\zhang\Desktop\无标题.xls'
+
 import json
 import os
 import openpyxl
+import requests
+
 
 path = r"C:\Users\zhang\Desktop"
 os.chdir(path)  # 修改工作路径
@@ -19,12 +19,28 @@ cell = sheet['E2:E1056']  # 获取A1到A5的数据
 
 list_data=[]
 day=0
+url="https://hear.abctime.com/v1/book/book-detail"
+header_dev={"Authorization":"Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxNTk0ODcwNzg0NzA4NTAxNTA1Iiwic3ViIjoie1wiaWRcIjoxNTk0ODcwNzg0NzA4NTAxNTA1LFwibW9iaWxlXCI6XCIrODYxNzM0NTA0MzM2OFwifSIsImV4cCI6MTY5Nzk1OTg1Nn0.wKCAumCwX_Ruhi9dmRhFcKYtUo1bvvgEsfvcgTJr_4i2RN_NvWcIENxooKZRcW8JW7b2a5go8076webN8On0qg"}
+UID=1547454324283875329
+book_id=1
+data_en={
+  "uid": UID,
+  "book_id": book_id
+}
+
 # 打印A1到A5的数据
 for i in cell:
     day+=1
     for j in i:
         json_data=json.loads(j.value)
         for ii in json_data["audio_book_ids"]:
+            book_id=ii
+            requestss=requests.post(url=url,json=data_en,headers=header_dev)
+            try:
+                if requestss.json()['code'] == str(300) or requestss.json()['data']=="Null" or requestss.json()['data']=="Null"or requestss.json()['data']['id']<=1:
+                    print("错误绘本，id：",ii)
+            except:
+                print("绘本正常，id",ii)
             if ii>2956:
             # if ii>2006:
                 print("第",day,"天，错误id",ii)
