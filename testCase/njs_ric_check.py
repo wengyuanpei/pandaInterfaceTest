a225cbac8df4e1d5e7845478282388b8857185de
wengyuanpei <wengyuanpei@163.com>
2023-05-11 17:27:54 +0800

    [interface] 英语dev计划脚本增加

--- testCase/njs_ric_check.py
+++ testCase/njs_ric_check.py
@@ -16,70 +16,73 @@ post_data={"resource_id":24,"uid":uid}
 
 #获取音频信息
 requestss=requests.post(url=dev_get_video_id_url,json=post_data,headers=header)
-print(requestss.json())
+# print(requestss.json())
 
 print(requestss.json()["data"]['lesson_list'][0]['medias_obj']['audio'])
 l1_video_id_list=requestss.json()["data"]['lesson_list'][0]['medias_obj']['audio']
-print("l1_video_id_list",requestss.json()["data"]['lesson_list'][1]['medias_obj']['audio'])
+# print("l1_video_id_list",requestss.json()["data"]['lesson_list'][1]['medias_obj']['audio'])
 l2_video_id_list=requestss.json()["data"]['lesson_list'][1]['medias_obj']['audio']
-print("l2_video_id_list",requestss.json()["data"]['lesson_list'][2]['medias_obj']['audio'])
+# print("l2_video_id_list",requestss.json()["data"]['lesson_list'][2]['medias_obj']['audio'])
 l3_video_id_list=requestss.json()["data"]['lesson_list'][2]['medias_obj']['audio']
-print("l3_video_id_list",requestss.json()["data"]['lesson_list'][3]['medias_obj']['audio'])
+# print("l3_video_id_list",requestss.json()["data"]['lesson_list'][3]['medias_obj']['audio'])
 l4_video_id_list=requestss.json()["data"]['lesson_list'][3]['medias_obj']['audio']
-print("l4_video_id_list",requestss.json()["data"]['lesson_list'][4]['medias_obj']['audio'])
+# print("l4_video_id_list",requestss.json()["data"]['lesson_list'][4]['medias_obj']['audio'])
 l5_video_id_list=requestss.json()["data"]['lesson_list'][4]['medias_obj']['audio']
-print("l5_video_id_list",requestss.json()["data"]['lesson_list'][5]['medias_obj']['audio'])
+# print("l5_video_id_list",requestss.json()["data"]['lesson_list'][5]['medias_obj']['audio'])
 l6_video_id_list=requestss.json()["data"]['lesson_list'][5]['medias_obj']['audio']
-print("l6_video_id_list",requestss.json()["data"]['lesson_list'][5]['medias_obj']['audio'])
-#
-# def HS_methd(req_sub_2):
-#     #传入请求统计行数（去除空行）
-#     txt = req_sub_2.text.splitlines(True)
-#     HS = len([l for l in txt if l.strip(' \n') != ''])
-#     return HS
-#
-# #id错误list
-# wrong_list=[]
-# #文件错误list
-# error_list=[]
-# #遍历音频
-# for L1video_id in l6_video_id_list:
-#
-#     dev_get_video_info1=dev_get_video_info+str(L1video_id)
-#
-#     req_get_video_info=requests.get(url=dev_get_video_info1,headers=header)
-#
-#     # print("音频id:",L1video_id,'音频地址',req_get_video_info.json()['data']['urls']['playUrls'][0]['playUrl'])
-#     #音频地址
-#     try:
-#         play_url=req_get_video_info.json()['data']['urls']['playUrls'][0]['playUrl']
-#
-#
-#
-#         # 音频英文字幕
-#         subtitleFileUrl_video=req_get_video_info.json()['data']['subtitleFileUrl']
-#         req_sub_1=requests.get(subtitleFileUrl_video)
-#         #计算行数
-#         txt_hs1 = HS_methd(req_sub_1)
-#
-#
-#
-#         #音频中文字幕
-#         secondaryFileUrl_video=req_get_video_info.json()['data']['secondaryFileUrl']
-#         req_sub_2 = requests.get(secondaryFileUrl_video)
-#         # 计算行数
-#         txt_hs2=HS_methd(req_sub_2)
-#         try:
-#             if txt_hs2==txt_hs1:
-#                 print(L1video_id,"音频通过")
-#         except:
-#             error_list.append(L1video_id)
-#             print(L1video_id, "音频不通过")
-#     except:
-#         wrong_list.append(L1video_id)
-#         print(L1video_id,"id请求错误需要检查")
-# print(wrong_list)
-# print(error_list)
+# print("l6_video_id_list",requestss.json()["data"]['lesson_list'][5]['medias_obj']['audio'])
+
+def HS_methd(req_sub_2):
+    #传入请求统计行数（去除空行）
+    txt = req_sub_2.text.splitlines(True)
+    HS = len([l for l in txt if l.strip(' \n') != ''])
+    return HS
+
+#id错误list
+wrong_list=[]
+#文件错误list
+error_list=[]
+#遍历音频
+for L1video_id in l1_video_id_list:
+
+    dev_get_video_info1=dev_get_video_info+str(L1video_id)
+
+    req_get_video_info=requests.get(url=dev_get_video_info1,headers=header)
+
+    # print("音频id:",L1video_id,'音频地址',req_get_video_info.json()['data']['urls']['playUrls'][0]['playUrl'])
+    #音频地址
+    try:
+        play_url=req_get_video_info.json()['data']['urls']['playUrls'][0]['playUrl']
+
+
+
+        # 音频英文字幕
+        subtitleFileUrl_video=req_get_video_info.json()['data']['subtitleFileUrl']
+        req_sub_1=requests.get(subtitleFileUrl_video)
+        # print('音频英文字幕',req_sub_1)
+        #计算行数
+        txt_hs1 = HS_methd(req_sub_1)
+        print('音频英文字幕', req_sub_1.text,"行数：",txt_hs1)
+
+
+
+        #音频中文字幕
+        secondaryFileUrl_video=req_get_video_info.json()['data']['secondaryFileUrl']
+        req_sub_2 = requests.get(secondaryFileUrl_video)
+        # 计算行数
+        txt_hs2=HS_methd(req_sub_2)
+        print('音频中文字幕', req_sub_2.text, "行数：", req_sub_2)
+        try:
+            if txt_hs2==txt_hs1:
+                print(L1video_id,"音频通过")
+        except:
+            error_list.append(L1video_id)
+            print(L1video_id, "音频不通过")
+    except:
+        wrong_list.append(L1video_id)
+        print(L1video_id,"id请求错误需要检查")
+print('id错误list',wrong_list)
+print('字幕错误错误list',error_list)
 
 
 
