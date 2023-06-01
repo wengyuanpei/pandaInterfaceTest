0ede1a855b2b3999dc55e50fcfaf3af6ab6423be
wengyuanpei <wengyuanpei@163.com>
2023-06-01 09:20:51 +0800

    [interface]完成计划

--- testCase/finish_en_play.py
+++ testCase/finish_en_play.py
@@ -8,14 +8,10 @@ url='https://hear-dev.abctime.com/hear-admin/v1/plan/finish-plan'
 
 header={"Authorization":"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJlbWFpbFwiOlwid2VuZ3l1YW5wZWlAZHJwYW5kYS5jb21cIixcImlkXCI6MjIzLFwibW9iaWxlXCI6XCIxNzM0NTA0MzM2NVwiLFwidXNlcm5hbWVcIjpcIndlbmd5dWFucGVpXCIsXCJ3b3JrY29kZVwiOlwiVjAwMTM3NTBcIixcInp5bE5hbWVcIjpcIjU3K0I2TCtjNlptcVwifSIsImV4cCI6MTY4NjExOTQ2M30.xaRUvC3OlcNGM29Lt_b48buQRWUyeANuQx48FRuO6PLPmZG3epFn0Qb2qr2yMF41S7ijQSPMMr8idGjtjNG9VA"}
 
-data1={"uid_string": "1607331624148455426", "subject": 1, "day": 59, "event_id": 1} # 绘本
-data2={"uid_string": "1607331624148455426", "subject": 1, "day": 59, "event_id": 2} #音频
 
+day=2
 
-
-day=59
-
-while day<1051+59:
+while day<3:
 
     data1 = {"uid_string": "1607331624148455426", "subject": 1, "day": day, "event_id": 1}  # 绘本
     data2 = {"uid_string": "1607331624148455426", "subject": 1, "day": day, "event_id": 2}  # 音频
