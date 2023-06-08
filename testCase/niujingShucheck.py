b5e682a9b0d01f972b0d1e2cefeebe876264c40c
wengyuanpei <wengyuanpei@163.com>
2023-06-08 19:44:02 +0800

    [interface]牛津树歌词检查

---
+++ testCase/niujingShucheck.py
@@ -0,0 +1,38 @@
+import requests
+
+
+header={"Authorization":"Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxNTYyNjI5MDYwMDc1MTAyMjA5Iiwic3ViIjoie1wiaWRcIjoxNTYyNjI5MDYwMDc1MTAyMjA5LFwibW9iaWxlXCI6XCIrODYxODM4NDI1MzUwNlwifSIsImV4cCI6MTcwMTY3NzU1M30.ByAdhAfbxwS5tTbkbSJIPJXN6bIrzoOjeWMwn6JA8pimm2v1fMTXVJfdXloqInXPY_FsTlc7ZPDwxlCGtFqQ5Q",
+        "User-Uid":"1562629060075102209",
+        "Kid-Uid":"1562629060075102209"}
+
+
+dataList={"resource_id":24,"uid":1562629060075102209}
+
+listUrl='https://hear-pre.abctime.com/v1/record/lesson/list'
+
+listreq=requests.post(url=listUrl,json=dataList,headers=header)
+level=1
+errorlist = []
+for lesson_list_num in range(len(listreq.json()['data']['lesson_list'])):
+
+    # print(listreq.json()['data']['lesson_list'][lesson_list_num]['medias_obj']['audio'])
+    level+=1
+    for audioID in listreq.json()['data']['lesson_list'][lesson_list_num]['medias_obj']['audio']:
+
+        getAudioInfoUrl='https://hear-pre.abctime.com/v1/media/audio/'+str(audioID)
+
+        getAudioInfo=requests.get(headers=header,url=getAudioInfoUrl)
+        # 歌词文件
+        print(getAudioInfo.json()['data']['subtitleFileUrl'])
+        #secondaryFileUrl
+        print(getAudioInfo.json()['data']['secondaryFileUrl'])
+
+        try:
+            if getAudioInfo.json()['data']['subtitleFileUrl']=="" or getAudioInfo.json()['data']['secondaryFileUrl'] =="":
+                print('等级',level,'音频id：',audioID,'功能异常！！')
+            else:
+                print('等级', level, '音频id：', audioID, 'OK！！')
+        except:
+            print('等级', level, '音频id：', audioID, '音频异常！！')
+
+print(errorlist)
