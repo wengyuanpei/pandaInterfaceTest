0d2d1213e092db473b9d745eeb8357636915c6dc
wengyuanpei <wengyuanpei@163.com>
2023-06-08 09:27:30 +0800

    [interface]单词检查是否扣词错误

--- testCase/TingXieWordsCheck.py
+++ testCase/TingXieWordsCheck.py
@@ -42,45 +42,48 @@ for grade_id in range(1,19):
 
         for i in range(len(GetDataGetRescourse.json()['data']['resource_list'])):
             # print(GetDataGetRescourse.json()['data']['resource_list'][i])
-            book_id=GetDataGetRescourse.json()['data']['resource_list'][i]['unit_id'] # 下个接口的bookid参数
+            try:
+                book_id=GetDataGetRescourse.json()['data']['resource_list'][i]['unit_id'] # 下个接口的bookid参数
 
 
-            publisher_idd=GetDataGetRescourse.json()['data']['resource_list'][i]['unit_id']
-            sleep(1)
-            # print('book_id',book_id)
+                publisher_idd=GetDataGetRescourse.json()['data']['resource_list'][i]['unit_id']
+                sleep(1)
+                # print('book_id',book_id)
 
-            # 选择单词
-            selectUrl = "https://hear-pre.abctime.com/v1/dictation/select"
-            selectData = {"book_id": book_id, "type": 1, "uid": 1562629060075102209}
-            selctreq = requests.post(json=selectData, url=selectUrl, headers=header)
-            sleep(1)
-            # print("选择单词：",selctreq.json()['data']['words_list'])
+                # 选择单词
+                selectUrl = "https://hear-pre.abctime.com/v1/dictation/select"
+                selectData = {"book_id": book_id, "type": 1, "uid": 1562629060075102209}
+                selctreq = requests.post(json=selectData, url=selectUrl, headers=header)
+                sleep(1)
+                # print("选择单词：",selctreq.json()['data']['words_list'])
 
-            #遍历保存出单词和单词ids
-            wordss=[]
-            wordIdss =[]
-            for words in range(len(selctreq.json()['data']['words_list'])):
-                wordsEnd=selctreq.json()['data']['words_list'][words]['word']
-                # print("单词:",wordsEnd)
-                wordss.append(wordsEnd)
-                wordidEnd = selctreq.json()['data']['words_list'][words]['word_id']
-                # print("单词id:", wordidEnd)
-                wordIdss.append(wordidEnd)
+                #遍历保存出单词和单词ids
+                wordss=[]
+                wordIdss =[]
+                for words in range(len(selctreq.json()['data']['words_list'])):
+                    wordsEnd=selctreq.json()['data']['words_list'][words]['word']
+                    # print("单词:",wordsEnd)
+                    wordss.append(wordsEnd)
+                    wordidEnd = selctreq.json()['data']['words_list'][words]['word_id']
+                    # print("单词id:", wordidEnd)
+                    wordIdss.append(wordidEnd)
 
-            sleep(1)
-            # 扣词接口
-            value=len(wordss)
-            deductionUrl = 'https://hear-pre.abctime.com/v1/dictation/deduction'
-            dataDeduction = {"pictureBookIds": [book_id], "value": 8,
-                    "word": wordss,
-                    "wordIds": wordIdss, "uid": 1562629060075102209}
+                sleep(1)
+                # 扣词接口
+                value=len(wordss)
+                deductionUrl = 'https://hear-pre.abctime.com/v1/dictation/deduction'
+                dataDeduction = {"pictureBookIds": [book_id], "value": 8,
+                        "word": wordss,
+                        "wordIds": wordIdss, "uid": 1562629060075102209}
 
-            deductionReq=requests.post(url=deductionUrl,json=dataDeduction,headers=header)
-            # print("扣词请求：",deductionReq.json())
-            # Errorlist=[]
-            if deductionReq.json()['code']=="200" :
-                print('年级：',grade_name, '教材：',JX, '单元：',book_id, '正常!')
-                print(deductionReq.json())
-            else:
-                print('年级：',grade_id, '教材：',JX, '单元：',book_id, '扣词异常!')
-
+                deductionReq=requests.post(url=deductionUrl,json=dataDeduction,headers=header)
+                # print("扣词请求：",deductionReq.json())
+                # Errorlist=[]
+                if deductionReq.json()['code']=="200" :
+                    print('年级：',grade_name, '教材：',JX, '单元：',book_id, '正常!')
+                    print(deductionReq.json())
+                else:
+                    print('年级：',grade_id, '教材：',JX, '单元：',book_id, '扣词异常!')
+            except:
+                print("异常请求",GetDataGetRescourse.json())
+                print('年级：', grade_name, '教材：', JX, '单元：', book_id)
