07eaa026875e2fc3514adfb963c18a0f162a2b75
wengyuanpei <wengyuanpei@163.com>
2023-06-13 14:44:59 +0800

    [interface]牛津树歌词检查

--- common/get_tlj_auth.py
+++ common/get_tlj_auth.py
@@ -45,3 +45,23 @@ def tljlogIn(phone_num_1):
 
     return auth_beare_token
 
+def tljlogInUid(phone_num_1):
+
+    # UG=user_agent()
+    # print("phone_num:",phone_num_1)
+    phone_num_end="+86"+str(phone_num_1)
+    # print("phone_num_end:",phone_num_end)
+    url_log="https://staging-subs.xiongmaoboshi.com/uis/ns/account/mobile-login-register-bind"
+    data_log={"bundleId":"com.drpanda.aural.launcher","code":"888888","key":phone_num_end}
+    header={"X-USER-API-VERSION":"3",}
+    req_log=requests.post(url_log,json=data_log,headers=header)
+    # auth_beare_token=req_log.headers["Authorization"]
+    uid=req_log.json()["data"]["id"]
+    # print(uid)
+    # tal_auth=req_log.headers['Tal-Token']
+    # print(tal_auth)
+    # print("uid:",uid)
+    # print(req_log.text)
+
+    return uid
+# tljlogInUid(17345011234)
--- testCase/niujingShucheck.py
+++ testCase/niujingShucheck.py
@@ -11,32 +11,51 @@ dataList={"resource_id":24,"uid":1562629060075102209}
 listUrl='https://hear-pre.abctime.com/v1/record/lesson/list'
 
 listreq=requests.post(url=listUrl,json=dataList,headers=header)
+print(listreq.json())
 level=1
+#没有返回字幕地址
 errorlist = []
+#字幕地址包含特殊字符无法解析
+wronglist=[]
 for lesson_list_num in range(len(listreq.json()['data']['lesson_list'])):
 
     # print(listreq.json()['data']['lesson_list'][lesson_list_num]['medias_obj']['audio'])
+    print(lesson_list_num)
 
     for audioID in listreq.json()['data']['lesson_list'][lesson_list_num]['medias_obj']['audio']:
-
+        print(audioID)
         getAudioInfoUrl='https://hear-pre.abctime.com/v1/media/audio/'+str(audioID)
-
+        # print(getAudioInfoUrl)
         getAudioInfo=requests.get(headers=header,url=getAudioInfoUrl)
         # 歌词文件
-        print(getAudioInfo.json()['data']['subtitleFileUrl'])
+        lrcEN=getAudioInfo.json()['data']['subtitleFileUrl']
+        # print(getAudioInfo.json()['data']['subtitleFileUrl'])
         #secondaryFileUrl
-        print(getAudioInfo.json()['data']['secondaryFileUrl'])
+        lrcCN=getAudioInfo.json()['data']['secondaryFileUrl']
+        # print(getAudioInfo.json()['data']['secondaryFileUrl'])
         name=getAudioInfo.json()['data']['name']
         sleep(1)
+        listStr=['!','@','#','&','%']
+
+        for i in listStr:
+
+            lrcCNfind1=str(lrcCN).find(i)
+            lrcCNfind11 = str(lrcEN).find(i)
+            if lrcCNfind1==0 or lrcCNfind11==0:
+                wronglist.append([level,audioID,name])
+                print('字幕地址包含特殊字符无法解析',lrcCNfind1)
+                print('字幕地址包含特殊字符无法解析',lrcCNfind11)
+
         try:
-            if getAudioInfo.json()['data']['subtitleText'] ==None or getAudioInfo.json()['data']['secondaryFileUrl'] ==None:
+            if str(lrcCN) == "" or str(lrcEN) == "" :
                 errorlist.append([level,audioID,name])
-                print('等级',level,'音频id：',audioID,'功能异常！！')
+                print('等级',level,'音频id：',audioID,'缺失链接字幕异常！！')
             else:
-                print('等级', level, '音频id：', audioID, 'OK！！')
+                print('等级', level, '音频id：', audioID, '链接OK！！')
         except:
-            print('等级', level, '音频id：', audioID, '音频异常！！')
+            print('等级', level, '音频id：', audioID, '音频链接地址返回异常！！')
     level += 1
 
 
-print(errorlist)
+print('没有返回字幕地址',errorlist)
+print('字幕地址包含特殊字符无法解析',wronglist)
