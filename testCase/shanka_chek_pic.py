cd3453efb490a0b0d08237d847f3930fd3f2073a
wengyuanpei <wengyuanpei@163.com>
2023-06-15 19:22:19 +0800

    [interface]闪卡图片翻译检查脚本

--- testCase/shanka_chek_pic.py
+++ testCase/shanka_chek_pic.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt # plt 用于显示图片
 from PIL import Image
 from testCase.translate import *
 
+from urllib import request, parse
+import json
+from faker import Faker
+
 wordList=[[41532,'strawberry','aa'],
 [41533,'lunch','aa'],
 [41534,'peach','aa'],
@@ -479,6 +483,45 @@ def getfiles():
     # print(filenames)
     return  wordlist,filenames
 
+
+class trans(object):
+    def __init__(self):
+        self.url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
+
+    def tran(self, text):
+        index = text.find("http")
+        text = text[:index]
+        text = text.replace('\n', '').replace('#', '').replace('RT ', '').replace(':', '')
+        ua = Faker().user_agent()
+        headers = {
+            'User-Agent': ua,
+            'Host': 'fanyi.youdao.com',
+            'Origin': 'http://fanyi.youdao.com',
+            'Referer': 'http://fanyi.youdao.com/',
+
+        }
+        # 表单数据
+        from_data = {
+            'i': text,
+            'from': 'UTO',
+            'to': 'UTO',
+            'smartresult': 'dict',
+            'client': 'fanyideskweb',
+            'doctype': 'json',
+            'version': '2.1',
+            'keyfrom': 'fanyi.web',
+            'action': 'FY_BY_REALTlME'
+        }
+        from_data = parse.urlencode(from_data).encode('utf-8')
+        req = request.Request(self.url, from_data, headers)
+        res = request.urlopen(req).read().decode("utf-8")
+        target = json.loads(res)
+        try:
+            result = target['translateResult'][0][0]['tgt']
+        except:
+            result = "Translate failed"
+        return result
+
 def runrun():
     for i in range(400):
         print("第%s张图片" % (i))
@@ -503,7 +546,7 @@ def runrun():
 #     print("第%s张图片" % (i))
 
 
-# 第二部查看图片
+# 第二部查看图片*********修改runrun方法里面的文件夹绝对路径
 if __name__ == "__main__":
     runrun()
 
