5536c7ae8a233783a3f9e1de4de698f1ac320bc5
wengyuanpei <wengyuanpei@163.com>
2023-07-28 17:22:56 +0800

    [add]fixxxx

--- testCase/runandreport.py
+++ testCase/runandreport.py
@@ -2,6 +2,15 @@ from common.emailfortest import *
 from testCase.servecereport import *
 from time import sleep
 
-runcase()
-sleep(5)
-sendemail()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    runcase()
+    sleep(5)
+    sendemail()
