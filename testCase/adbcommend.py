e78fcf54d0433e158d5ffd43da23ef0702209c10
wengyuanpei <wengyuanpei@163.com>
2023-09-06 18:34:39 +0800

    [add]fixxxx

--- testCase/adbcommend.py
+++ testCase/adbcommend.py
@@ -7,11 +7,21 @@ def execute(cmd):
 
 
 if __name__ == '__main__':
-    # 完整命令的
+    # 执行动作亮屏-解锁-锁定-解锁 循环
     a=1
     while True:
-        sleep(1)
+
         print('第%d次执行锁屏命令！' % a)
         com="adb shell input keyevent 26"
         execute(com)
+        #解锁
+
+        open='adb shell input swipe 148 1015 563 1046'
+        execute(open)
+        sleep(0.5)
+
+        execute(com)
+        execute(com)
+
+        sleep(2)
         a+=1
