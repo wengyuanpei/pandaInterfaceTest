8674a260dcbd1ce61a6d7e3ab44b410dc5d301e9
weng <wengyuanpei@163.com>
2023-11-21 09:58:44 +0800

    [add]添加ABCdev环境签名生成方法

---
+++ testCase/xmindToTestlink.py
@@ -0,0 +1,18 @@
+
+
+
+'''
+
+xmind 格式转xmind
+
+
+'''
+import os
+
+textName='xmind.xmind'
+path=os.path.abspath('xmind.xmind')
+print('文件路径',path)
+commond='xmind2testlink '+path
+os.system(commond)
+
+
