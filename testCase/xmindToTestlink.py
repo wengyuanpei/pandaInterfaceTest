2bb67303577c9dadff6fe74eadc5a6b677322fb9
weng <wengyuanpei@163.com>
2023-11-23 17:28:15 +0800

    [add]xmind 文件转为xml文件

--- testCase/xmindToTestlink.py
+++ testCase/xmindToTestlink.py
@@ -1,22 +1,17 @@
 
 
+#xmind 格式转xmind
+#文件打包命令 -i 使用图标
 
-'''
+#Pyinstaller -F -i C:\Users\zhang\Desktop\捕获.PNG  xmindToTestlink.py
 
-xmind 格式转xmind
-文件打包命令 -i 使用图标
-Pyinstaller -F -i C:\Users\zhang\Desktop\捕获.PNG  xmindToTestlink.py
-
-'''
 import os
 
 
-path=input("请输入文件的绝对路径如 C:data/a.xmind : " )
-if path=='':
-    print('未输入内容！请重新输入')
-    path = input("请输入文件的绝对路径如：C:data/a.xmind")
-print('文件路径',path)
-commond='xmind2testlink '+path
+pathData=input("请输入文件的绝对路径如 C:data/a.xmind : ")
+# patnSave=input("请输入文件的绝对路径如 C:data/a.xmind : ")
+print('文件路径',pathData)
+commond='xmind2testlink '+pathData
 os.system(commond)
 
 
