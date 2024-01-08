09382f279cf18a17bfc8277976de18aea3838ab8
weng <wengyuanpei@163.com>
2024-01-08 19:38:45 +0800

    [add] 学习计划定级接口

--- testCase/abcReading/checkReport.py
+++ testCase/abcReading/checkReport.py
@@ -1,8 +1,7 @@
 import requests
-
 from common.abcSign import *
 from common.abcReqHeader import abcBaseUrl
-
+from time import sleep
 
 baseurl=abcBaseUrl('live')
 enverment='live'
@@ -30,10 +29,11 @@ if __name__ == '__main__':
     uidlist=[16290218]
     errorlist=[]
     for uid in uidlist:
+        sleep(5)
         oldNUm=getOldReport(uid)
         newNUM=getNueReport(uid)
         print('老版本阅读绘本数据%d,新版本阅读绘本数%d'%(oldNUm,newNUM))
-        if oldNUm >=newNUM:
-            print('异常账号'+str(uid))
+        if oldNUm >newNUM:
+            print('############################异常账号'+str(uid)+'################################')
             errorlist.append(uid)
     print(errorlist)
