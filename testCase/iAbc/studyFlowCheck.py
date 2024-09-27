97b6f262aa083fbfbd37e366239d3b99308094e1
wengyuanpei <v_wengyuanpei@tal.com>
2024-09-27 17:47:19 +0800

    studyflow学习流表处理

--- testCase/iAbc/studyFlowCheck.py
+++ testCase/iAbc/studyFlowCheck.py
@@ -1,10 +1,9 @@
 from common.excelreadwrite import *
 
 
-
-
 def studyFlowExcelInfo(path,excelposition):
     excel=excel_read(path,excelposition)
+    Excel_end=excel.remove(2)
     print(excel)
 
 
