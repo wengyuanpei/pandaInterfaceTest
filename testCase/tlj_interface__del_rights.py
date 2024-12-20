a5be47bd4a44164cb309ff7d5c4bccf307a4f8a0
wengyuanpei <v_wengyuanpei@tal.com>
2024-12-20 19:35:05 +0800

    feature：fix

--- testCase/tlj_interface__del_rights.py
+++ testCase/tlj_interface__del_rights.py
@@ -13,13 +13,14 @@ url="https://pad-dev.xiongmaoboshi.com/s/device/hearing/rights/manufacturer/data
 list=['{S1TESTLY015}','{S01D1042320702478}']
 
 #听力机的令牌
-auth_b=getauth()[1]
+phone=phone_num()
+auth_b=getauth(phone)[1]
 # print(auth)
 
 #ola 鉴权
 auth_ht='Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJlbWFpbFwiOlwid2VuZ3l1YW5wZWlAZHJwYW5kYS5jb21cIixcImlkXCI6MjIzLFwibW9iaWxlXCI6XCIxNzM0NTA0MzM2NVwiLFwidXNlcm5hbWVcIjpcIndlbmd5dWFucGVpXCJ9IiwiZXhwIjoxNjc4MzU4NDIzfQ.QuCZd_AEXaONUu9jOVoDMSmNuIekU-K6rKIbpFzMBj5V1GdW0UYNVE3Kx35iq8jcW2-bMz7BQ-TbpTUKA1oPqg'
 header={
-            "Authorization": auth_ht,
+            "Authorization": auth_b,
             "Content-Type": "application/json;charset=UTF-8"
         }
 for sn in list:
