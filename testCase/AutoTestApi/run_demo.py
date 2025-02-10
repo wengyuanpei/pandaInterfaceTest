76ecebf7daf752e02a7fa5f7b8d3428c6a277e7f
wengyuanpei <v_wengyuanpei@tal.com>
2025-02-10 18:21:38 +0800

    feature：AutoTestApiDemo

---
+++ testCase/AutoTestApi/run_demo.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+__author__ = 'weng'
+
+
+import os,sys
+sys.path.append(os.path.dirname(__file__))
+from testCase.AutoTestApi.config import setting
+import unittest,time
+from testCase.AutoTestApi.lib.sendmail import send_mail
+from testCase.AutoTestApi.lib.newReport import new_report
+# from db_fixture import test_data
+from testCase.AutoTestApi.package.HTMLTestRunner import HTMLTestRunner
+
+def add_case(test_path=setting.TEST_CASE):
+    """加载所有的测试用例"""
+    discover = unittest.defaultTestLoader.discover(test_path, pattern='*API.py')
+    return discover
+
+def run_case(all_case, result_path=setting.TEST_REPORT):
+    """执行所有的测试用例"""
+
+    # 初始化接口测试数据
+    # test_data.init_data()
+
+    now = time.strftime("%Y-%m-%d %H_%M_%S")
+    filename = result_path + '/' + now + 'result.html'
+    fp = open(filename,'wb')
+    #测试环境展示在报告中
+    runner = HTMLTestRunner(stream=fp, title='demo接口自动化测试报告',
+                            description='环境：windows 7 浏览器：chrome',
+                            tester='weng')
+    runner.run(all_case)
+    fp.close()
+    report = new_report(setting.TEST_REPORT) #调用模块生成最新的报告
+
+    send_mail(report) #调用发送邮件模块
+
+if __name__ =="__main__":
+    def runcase():
+        cases = add_case()
+        run_case(cases)
+
+
+    if __name__ == '__main__':
+        runcase()
+    # 定时器定时执行任务，根据业务调整定时器
+    # timeSc=0
+    # timesss=0
+    # while True:
+    #     sleep(100)
+    #     timeSc+=1
+    #     if timeSc % 36 ==0:
+    #         runcase()
+    #         timesss+=1
+    #         print('执行了次！',str(timesss),'次')
+
+
+
+
+
