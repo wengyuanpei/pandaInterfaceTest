5932079f1dc7509d02e109ff7fc7a5159c8c1197
Administrator <wengyuanpei@163.com>
2025-07-04 23:31:30 +0800

    增加合并数据表的方法

--- testCase/ttl2D/studyPlanCompeare.py
+++ testCase/ttl2D/studyPlanCompeare.py
@@ -3,7 +3,9 @@ from common.excelreadwrite import excel_group
 def get_excel_date():
     groupp=['book_id', 'unit_number', 'day', 'stage', 'unit_id']
     groupby='content'
-    result_data=excel_group('ttlstudydate.xlsx','Sheet1', groupp,groupby)
+    excelpath='ttlstudydate.xlsx'
+    sheetname='Sheet1'
+    result_data=excel_group(excelpath,sheetname,groupp,groupby)
     return result_data
 #获取接口返回的每日计划
 def get_plan_interfacedate():
